afcfe5330d67ee3544f6cc914ba082995146249a
oscar.vega <oscar.vega@atomicware.mx>
2015-06-04 17:48:17 -0500

    cambios conexion server

--- config/connections.js
+++ config/connections.js
@@ -48,46 +48,13 @@ module.exports.connections = {
     password: 'miskupones',
     database: 'mis_kupones'
   },
-
-  /***************************************************************************
-  *                                                                          *
-  * MongoDB is the leading NoSQL database.                                   *
-  * http://en.wikipedia.org/wiki/MongoDB                                     *
-  *                                                                          *
-  * Run: npm install sails-mongo                                             *
-  *                                                                          *
-  ***************************************************************************/
-  someMongodbServer: {
-    adapter: 'sails-mongo',
+  prod_kuponesServer: {
+    adapter: 'sails-mysql',
     host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    user: 'mavergar_dev',
+    password: 'dev2015',
+    database: 'dev_kupones'
   },
 
-  /***************************************************************************
-  *                                                                          *
-  * PostgreSQL is another officially supported relational database.          *
-  * http://en.wikipedia.org/wiki/PostgreSQL                                  *
-  *                                                                          *
-  * Run: npm install sails-postgresql                                        *
-  *                                                                          *
-  *                                                                          *
-  ***************************************************************************/
-  somePostgresqlServer: {
-    adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB'
-  }
-
-
-  /***************************************************************************
-  *                                                                          *
-  * More adapters: https://github.com/balderdashy/sails                      *
-  *                                                                          *
-  ***************************************************************************/
 
 };
--- config/env/production.js
+++ config/env/production.js
@@ -17,9 +17,10 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+    models: {
+     connection: 'prod_kuponesServer',
+     migrate: 'alter' 
+   },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *

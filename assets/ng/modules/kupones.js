384ab06487e0f1d425d6a84d78675fd5ab8fed2d
lentiummmx <lentium.mmx@gmail.com>
2015-06-04 16:29:02 -0500

    Se aplican cambios para Kupones y Categorias/SubCategorias

--- assets/ng/modules/kupones.js
+++ assets/ng/modules/kupones.js
@@ -380,7 +380,7 @@ module.controller('KuponesCtrl', function($scope, $location, $http, $route, $rou
 			// this callback will be called asynchronously
 			// when the response is available
 				cb(null, data);
-			}).error(function(err, status, headers, config) {
+			}).error(function(error, status, headers, config) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
 				cb(error);
